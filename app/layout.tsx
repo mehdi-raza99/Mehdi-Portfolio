e2a00e896083336e27333b90850e36a5c13889da
Mehdi Raza <mehdics100@gmail.com>
2025-09-01 15:54:53 +0500

    Resume updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Mehdi's Portfolio",
+  description: "Created with v0",
+  generator: "v0.app",
+  icons: {
+    icon: "/icon.ico", // Path to your favicon in the public directory
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -20,5 +23,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
